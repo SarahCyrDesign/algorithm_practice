21eba5e284b9c56e642fe4e4616e645a6c8fb5a2
Sarah Cyr Design <sarahcyrdesign@gmail.com>
2017-12-20 18:55:46 -0500

    Add solution to simple array rotation

--- javascript/7kyu-simple-array-rotation.js
+++ javascript/7kyu-simple-array-rotation.js
@@ -1,58 +1,31 @@
-// function solve(arr) {
-//   for (i = 0; i < arr. length; i++) {  //loops through the array
-//     if (arr[i] < arr[i+1] && arr[i+1] < arr[i+2]) {
-//       return 'A'
-//     } else if (arr[i] > arr[i+1] && arr[i+1] > arr[i+2]) {
-//       return 'D'
-//     } else if (arr[i] > arr[i+1] && arr[i+1] < arr[i+2]) {
-//       return 'RA'
-//     } else if (arr[i] < arr[i+1] && arr[i+1] > arr[i+2]) {
-//       return 'RD'
-//     }
-//   }
-// }
-
 function solve(arr) {
-  for (i = 0; i < arr. length; i++) {
-    let left = arr[0];
-    let right = arr.length - 1;
-    let middle = left + (left + right) / 2;
+  var index0 = 0;
+  var index1 = 0;
 
-     if (left < right && middle < right) {
-      return 'A'
-    } else if (left > right && middle > right) {
-      return 'D'
-    } else if (left > right && middle < right) {
-      return 'RA'
-    } else if (left < right && middle > right) {
-      return 'RD'
+  for(i = 1; i < arr.length; i++) {
+
+    if(arr[i] > arr[index1]) {
+    index1 = i;
+  }
+
+    if(arr[i] < arr[index0]) {
+      index0 = i;
     }
-
   }
 
+  if(index1 == 0 && index0 == arr.length - 1) {
+    return "D";
+  } else if(index1 == arr.length - 1 && index0 == 0) {
+    return "A";
+  } else if(index0 > index1) {
+    return "RA";
+  } else if(index1 > index0){
+   return "RD";
   }
+}
 
-
-
-//   function solve(arr) {
-//   for (i = 0; i < arr. length; i++) {
-//   let left = arr[0];
-//   let right = arr.length - 1;
-
-//   while (arr[left] > arr[right])
-//   let middle = left + (left + right) / 2;
-
-//   {
-
-//     if(arr[middle] > arr[right])
-//       {
-//         left = middle + 1;
-//       }
-//       else
-//       {
-//         right = middle;
-//       }
-//     }
-//       return left;
-//   }
-  }
+//start each index at 0
+//loop through the array
+//if the current index is more than index[1] assign index[1] to the current index
+//if the current index is less than index[0] assign index[0] to the current index
+//compare index[0] and index[1] to determine if the array elements are ascending, descending , rotated ascending or rotated descending in their positions
